52897896de5bca6e5651b031372d3d21516137f9
Herbert Lange <sonofchaosgmx.de>
2012-06-09 17:14:41 +0200

    First step to fix language recognition

--- Language.hpp
+++ Language.hpp
@@ -5,74 +5,22 @@
 #include <fstream>
 #include <iostream>
 #include <unordered_map>
+#include <set>
+#include <sstream>
+#include "Util.hpp"
 
 #define TOP_FACTOR 10 
 using namespace std;
 
-class State;
-
-class Transition {
-  public:
-    State *target;
-    set<string> labels;
-};
-
-class State {
-  public:
-    State(string value);
-    set<string> values;
-    vector<Transition *> arcs;
-};
-
-class Automaton {
-  public:
-    State start_state;
-    void add(string language,string label);
-};
-
 class LanguageRecognizer {
   public:
-    void training(string language,string filename);
-    void training(string language,ifstream *in);
-    string recognize(string line);
+    void training(string language, string filename);
+    void training(string language, ifstream *in);
+    set<string> *recognize(string line);
+    set<string> *recognize(istream* in);
   private:
-    Automaton knowsyourlanguage;
+  unordered_map<string,set<string>> knowsyourlanguage;
 };
-
-Transition::Transition(string label, State *t)
-{
-  labels.insert(label);
-  target=t;
-}
-
-State::State(string value)
-{
-  values.insert(value);
-}
-
-void Automaton::add(string language, string label)
-{
-  // Empty automaton
-  if(start_state.transitions.size()==0)
-    {
-      start_state.transitions.push_back(new Transition(string label, new State(language)));
-    }
-  else
-    {
-      for (auto it=start_state.transitions.begin() ; it < start_state.transitions.end(); it++ )
-	{
-	  // Already label of a transition to a different language
-	  if ((it->labels.count(label)>0)&&(it->target->values.count(language)==0)) 
-	    {
-	      State *s=new State();
-	      s->values=it->target->values;
-	      s->values.insert(language);
-
-	      start_state.transitions.push_back(new Transition(
-	    }
-	}
-    }
-}
 
 void LanguageRecognizer::training(string language, string filename)
 {
@@ -88,20 +36,36 @@ void LanguageRecognizer::training(string language, string filename)
 
 void LanguageRecognizer::training(string language, ifstream *in)
 {
+#ifdef DEBUG
+  cout << "Start training for " << language << endl;
+#endif
+  // Generate complete trigram frequency list
   unordered_map<string,int> trigram_freq;
   while(!in->eof())
     {
       string word;
-      cin >> word;
-      for(int pos=0;pos<word.length()-2;pos++)
+      *in >> word;
+      string nword=normalize(word);
+      if (nword.length()<3)
 	{
-	  string tri=word.substr(pos,3);
-	  trigram_freq[tri]++;
+	  trigram_freq[nword]++;
 #ifdef DEBUG
-	  cout << tri << endl;
+	  cout << nword << endl;
 #endif
 	}
+      else
+	{
+	  for(int pos=0;pos<nword.length()-2;pos++)
+	    {
+	      string tri=nword.substr(pos,3);
+	      trigram_freq[tri]++;
+#ifdef DEBUG
+	      cout << tri << endl;
+#endif
+	    }
+	}
     }
+  // Trim result list 
   size_t toplen=trigram_freq.size()/TOP_FACTOR;
   pair<string,int> top_list[toplen];
   for ( auto it = trigram_freq.begin(); it != trigram_freq.end(); ++it )
@@ -112,17 +76,77 @@ void LanguageRecognizer::training(string language, ifstream *in)
 	{
 	  if (top_list[pos].second<smallest)
 	    {
-	      smallest=top_list[pos];
+	      smallest=top_list[pos].second;
 	      smallest_pos=pos;
 	    }
 	}
       if (top_list[smallest_pos].second<it->second)
 	{
+	  // Set trigram to new value
 	  top_list[smallest_pos].first=it->first;
+	  // Set count to new value
 	  top_list[smallest_pos].second=it->second;
 	  break;
 	}
     }
+  // Store trimmed list with language
+  for(int pos=0;pos<toplen;pos++)
+    {
+      knowsyourlanguage[top_list[pos].first].insert(language);
+    }
+}
+
+set<string> *LanguageRecognizer::recognize(string line)
+{
+  stringstream linestream(line,ios::in);
+  recognize((istream *)&linestream);
+}
+
+set<string> *LanguageRecognizer::recognize(istream *in)
+{
+  unordered_map<string,int> lang_count;
+  // Count all the trigrams
+  while(!in->eof())
+    {
+      string word;
+      *in >> word;
+      string nword=normalize(word);
+      if (nword.length()<3)
+	{
+	  for ( auto it = knowsyourlanguage[nword].begin(); it != knowsyourlanguage[nword].end(); ++it )
+	    lang_count[*it]++;
+#ifdef DEBUG
+	  cout << nword << endl;
+#endif
+	}
+      else
+	{
+	  for(int pos=0;pos<word.length()-2;pos++)
+	    {
+	      string tri=word.substr(pos,3);
+	      for ( auto it = knowsyourlanguage[tri].begin(); it != knowsyourlanguage[tri].end(); ++it )
+		lang_count[*it]++;
+#ifdef DEBUG
+	      cout << tri << endl;
+#endif
+	    }
+	}
+    }
+  // Find maximum trigram count
+  int maxcount=0;
+  for ( auto it = lang_count.begin(); it != lang_count.end(); ++it )
+    {
+      if (it->second>maxcount)
+	maxcount=it->second>maxcount;
+    }
+  // Find all languages with maximum trigram count
+  set<string> *res=new set<string>();
+  for ( auto it = lang_count.begin(); it != lang_count.end(); ++it )
+    {
+      if (it->second==maxcount)
+	res->insert(it->first);
+    }
+  return res;
 }
 
 #endif
