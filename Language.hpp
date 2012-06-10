867df1ca115027dd969f3c293081a96c82b09e9a
Herbert Lange <sonofchaosgmx.de>
2012-06-11 00:01:53 +0200

    Finally made a working language recognition in Language.hpp. Still needs to be integrated into the search engine

--- Language.hpp
+++ Language.hpp
@@ -7,9 +7,10 @@
 #include <unordered_map>
 #include <set>
 #include <sstream>
+#include <climits>
 #include "Util.hpp"
 
-#define TOP_FACTOR 10 
+#define TOP_FACTOR 10
 using namespace std;
 
 class LanguageRecognizer {
@@ -48,31 +49,39 @@ void LanguageRecognizer::training(string language, ifstream *in)
       string nword=normalize(word);
       if (nword.length()<3)
 	{
-	  trigram_freq[nword]++;
+	  if (nword.length()>0)
+	    trigram_freq[nword]++;
 #ifdef DEBUG
 	  cout << nword << endl;
 #endif
 	}
       else
 	{
-	  for(int pos=0;pos<nword.length()-2;pos++)
+	  for(unsigned int pos=0;pos<nword.length()-2;pos++)
 	    {
 	      string tri=nword.substr(pos,3);
 	      trigram_freq[tri]++;
 #ifdef DEBUG
-	      cout << tri << endl;
+	      cout << tri << "\t" << trigram_freq[tri] << endl;
+	      //	      cout << "Got " << trigram_freq.size() << " elements in trigram frequency list" << endl;  
 #endif
 	    }
 	}
     }
   // Trim result list 
-  size_t toplen=trigram_freq.size()/TOP_FACTOR;
+  size_t toplen=trigram_freq.size()*TOP_FACTOR/100;
+#ifdef DEBUG
+  cout << "Keeping top " << toplen << " of " << trigram_freq.size() << " elements" << endl;
+#endif
   pair<string,int> top_list[toplen];
   for ( auto it = trigram_freq.begin(); it != trigram_freq.end(); ++it )
     {
+#ifdef DEBUG
+      cout << "Iterator " << it->first << "\t" << it->second << endl;
+#endif
       int smallest_pos=0;
-      int smallest=0;
-      for(int pos=0;pos<toplen;pos++)
+      int smallest=INT_MAX;
+      for(unsigned int pos=0;pos<toplen;pos++)
 	{
 	  if (top_list[pos].second<smallest)
 	    {
@@ -86,24 +95,38 @@ void LanguageRecognizer::training(string language, ifstream *in)
 	  top_list[smallest_pos].first=it->first;
 	  // Set count to new value
 	  top_list[smallest_pos].second=it->second;
-	  break;
+#ifdef DEBUG
+	  cout << "Setting position " << smallest_pos << " to " << top_list[smallest_pos].first << " with count " << top_list[smallest_pos].second << endl;
+#endif
 	}
     }
   // Store trimmed list with language
-  for(int pos=0;pos<toplen;pos++)
+  for(unsigned int pos=0;pos<toplen;pos++)
     {
+#ifdef DEBUG
+      //      cout << "Got " << knowsyourlanguage.size() << " elements in trigram list" << endl;  
+      cout << "Adding "<< top_list[pos].first << " with language " << language << endl;  
+#endif
       knowsyourlanguage[top_list[pos].first].insert(language);
     }
+
 }
 
 set<string> *LanguageRecognizer::recognize(string line)
 {
+#ifdef DEBUG
+  cout << "Got " << knowsyourlanguage.size() << " elements in trigram list" << endl;
+  cout << "Recognizing string :" << endl << line << endl;
+#endif
   stringstream linestream(line,ios::in);
-  recognize((istream *)&linestream);
+  return recognize((istream *)&linestream);
 }
 
 set<string> *LanguageRecognizer::recognize(istream *in)
 {
+#ifdef DEBUG
+  cout << "Recognizing instream :" << endl;
+#endif
   unordered_map<string,int> lang_count;
   // Count all the trigrams
   while(!in->eof())
@@ -113,22 +136,29 @@ set<string> *LanguageRecognizer::recognize(istream *in)
       string nword=normalize(word);
       if (nword.length()<3)
 	{
-	  for ( auto it = knowsyourlanguage[nword].begin(); it != knowsyourlanguage[nword].end(); ++it )
-	    lang_count[*it]++;
+	  if (nword.length()>0)
+	    {
+	      for ( auto it = knowsyourlanguage[nword].begin(); it != knowsyourlanguage[nword].end(); ++it )
+		{
+		  lang_count[*it]++;
 #ifdef DEBUG
-	  cout << nword << endl;
+		  cout << nword << "\t" << *it << "\t" << lang_count[*it] << "#"<< endl;
 #endif
+		}
+	    }
 	}
       else
 	{
-	  for(int pos=0;pos<word.length()-2;pos++)
+	  for(unsigned int pos=0;pos<word.length()-2;pos++)
 	    {
 	      string tri=word.substr(pos,3);
 	      for ( auto it = knowsyourlanguage[tri].begin(); it != knowsyourlanguage[tri].end(); ++it )
-		lang_count[*it]++;
+		{
+		  lang_count[*it]++;
 #ifdef DEBUG
-	      cout << tri << endl;
+		  cout << tri << "\t" << *it << "\t" << lang_count[*it] << endl;
 #endif
+		}
 	    }
 	}
     }
@@ -137,15 +167,29 @@ set<string> *LanguageRecognizer::recognize(istream *in)
   for ( auto it = lang_count.begin(); it != lang_count.end(); ++it )
     {
       if (it->second>maxcount)
-	maxcount=it->second>maxcount;
+	maxcount=it->second;
     }
+#ifdef DEBUG
+  cout << "Maximum per-language trigram count " << maxcount << endl;
+#endif
   // Find all languages with maximum trigram count
   set<string> *res=new set<string>();
+#ifdef DEBUG
+  cout << "Possible language candidates: ";
+#endif
   for ( auto it = lang_count.begin(); it != lang_count.end(); ++it )
     {
       if (it->second==maxcount)
-	res->insert(it->first);
+	{
+	  res->insert(it->first);
+#ifdef DEBUG
+	  cout << it-> first;
+#endif
+	}
     }
+#ifdef DEBUG 
+  cout << endl;
+#endif
   return res;
 }
 
