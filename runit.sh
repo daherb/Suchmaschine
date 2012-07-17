02576410a08f6a6413be7ea6bb05a990781affc8
Herbert Lange <sonofchaosgmx.de>
2012-07-17 21:00:05 +0200

    Removed unneccessary help script

--- runit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-./main -add ../Daten/1.txt  -add ../Daten/2.txt -add ../Daten/3.txt  -add ../Daten/4.txt  -add ../Daten/5.txt \
- -add ../Daten/6.txt -add ../Daten/7.txt -add ../Daten/8.txt -add ../Daten/9.txt  -add ../Daten/10.txt \
- -add ../Daten/11.txt -add ../Daten/12.txt -add ../Daten/13.txt -add ../Daten/14.txt  -add ../Daten/15.txt \
- -add ../Daten/16.txt -add ../Daten/17.txt -add ../Daten/18.txt -add ../Daten/19.txt  -add ../Daten/20.txt  \
- -store index.txt
