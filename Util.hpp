6ebb5b16de38a489fcd8fed9820bcc55c24c43cc
Herbert Lange <sonofchaosgmx.de>
2012-10-23 15:10:26 +0200

    Some fiexs concerning normalisation

--- Util.hpp
+++ Util.hpp
@@ -48,10 +48,14 @@ string normalize(string word)
 	      // Skip all Special characters in this block
 	    case 0xc2:
 	      break;
-	      // Normalize umlaut
+	      // Normalize diacritics and special characters
 	    case 0xc3:
 	      switch((unsigned char)word[pos+1])
 		{
+		  // Remove some special characters
+		case 0x97:
+		case 0xb7:
+		  break;
 		  // Remove all diacritics form a's
 		case 0x80:
 		case 0x81:
@@ -59,24 +63,85 @@ string normalize(string word)
 		case 0x83:
 		case 0x84:
 		case 0x85:
-		case 0x86:
 		case 0xa0:
 		case 0xa1:
 		case 0xa2:
 		case 0xa3:
 		case 0xa4:
 		case 0xa5:
-		case 0xa6:
 		  result+='a';
 		  break;
+		  // Remove all diacritics form e's
+		case 0x88:
+		case 0x89:
+		case 0x8a:		
+		case 0x8b:
+		case 0xa8:
+		case 0xa9:
+		case 0xaa:		
+		case 0xab:
+		  result+='e';
+		  break;
+		  // Remove all diacritics form i's
+		case 0x8c:
+		case 0x8d:
+		case 0x8e:
+		case 0x8f:
+		case 0xac:
+		case 0xad:
+		case 0xae:
+		case 0xaf:
+		  result+='i';
+		  break;
+		  // Remove all diacritics form o's
+		case 0x92:
+		case 0x93:
+		case 0x94:
+		case 0x95:
 		case 0x96:
+		case 0x98:
+		case 0xb2:
+		case 0xb3:
+		case 0xb4:
+		case 0xb5:
 		case 0xb6:
+		case 0xb8:
 		  result+='o';
+		  break;
+		  // Remove all diacritics form u's
+		case 0x99:
+		case 0x9a:
+		case 0x9b:
 		case 0x9c:
+		case 0xb9:
+		case 0xba:
+		case 0xbb:
 		case 0xbc:
 		  result+='u';
+		  // Remove all diacritics form c's
+		case 0x87:
+		case 0xa7:
+		  result+='c';
+		  break;
+		  // Remove all diacritics form n's
+		case 0x91:
+		case 0xb1:
+		  result+='n';
+		  break;
+		  // Remove all diacritics form y's
+		case 0x9d:
+		case 0xbd:
+		case 0xbf:
+		  result+='y';
+		  break;
+		  // Split ligatures
 		case 0x9f:
 		  result+="ss";
+		  break;
+		case 0x86:
+		case 0xa6:
+		  result+="ae";
+		  break;
 		}
 	      break;
 	    default:
